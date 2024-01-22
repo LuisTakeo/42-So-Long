c97000b4b53d67d8e5a8c67ec16c70e3e27dbe58
LuisTakeo <luistakeo@hotmail.com>
2024-01-21 22:06:30 -0300

    Inserido hook de teclas a pressionar e criado Structs para estrutura do jogo

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,16 +6,33 @@
 /*   By: tpaim-yu <tpaim-yu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 20:03:36 by tpaim-yu          #+#    #+#             */
-/*   Updated: 2024/01/18 17:08:22 by tpaim-yu         ###   ########.fr       */
+/*   Updated: 2024/01/21 21:55:17 by tpaim-yu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
+# define WIDTH_TILE 128
+# define HEIGHT_TILE 128
 
 # include "MLX42/MLX42.h"
 # include "../lib/libft/libft.h"
 # include "../lib/libft/gnl/includes/get_next_line.h"
 # include "../lib/libft/ft_printf/includes/ft_printf.h"
+
+typedef struct s_image_data
+{
+	mlx_image_t		*img;
+	mlx_texture_t	*texture;
+}	t_img_data;
+
+typedef struct s_game
+{
+	mlx_t	*mlx;
+	t_img_data	*wall_data;
+	t_img_data	*floor_data;
+	t_img_data	*character_data;
+	t_img_data	*collectible_data;
+}	t_game;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tpaim-yu <tpaim-yu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 20:02:36 by tpaim-yu          #+#    #+#             */
-/*   Updated: 2024/01/21 18:31:23 by tpaim-yu         ###   ########.fr       */
+/*   Updated: 2024/01/21 22:05:07 by tpaim-yu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,49 +30,66 @@ static void	ft_error(void)
 // 	ft_printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
 // }
 
-void	my_keyhook(mlx_key_data_t keydata, void* param)
+void	my_keyhook(mlx_key_data_t keydata, void *param)
 {
-	// If we PRESS the 'J' key, print "Hello".
+	t_img_data	*img_data;
+
+	img_data = (t_img_data *)param;
 	if (keydata.key == MLX_KEY_A && keydata.action == MLX_PRESS)
-		ft_printf("Hello ");
-
-	// If we RELEASE the 'K' key, print "World".
-	if (keydata.key == MLX_KEY_S && keydata.action == MLX_RELEASE)
-		ft_printf("World");
-
-	// If we HOLD the 'L' key, print "!".
-	if (keydata.key == MLX_KEY_D && keydata.action == MLX_REPEAT)
-		ft_printf("!");
-	if (param)
+		ft_printf("Key pressed: %c\n", keydata.key);
+	if (keydata.key == MLX_KEY_S && keydata.action == MLX_PRESS)
+	{
+		ft_printf("Key pressed: %c\n", keydata.key);
+	}
+	if (keydata.key == MLX_KEY_D && keydata.action == MLX_PRESS)
+		ft_printf("Key pressed: %c\n", keydata.key);
+	if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS)
+		ft_printf("Key pressed: %c\n", keydata.key);
+	if (img_data)
 		return ;
 }
 
+// teste de loop para animações no bonus
+// void	ft_count_loop(void *i)
+// {
+// 	int32_t	*it;
+
+// 	it = i;
+// 	(*it)++;
+// 	if(*it > 100)
+// 		*it = 0;
+// 	ft_printf("%d \n", *it);
+// }
+
 int32_t	main(void)
 {
-	mlx_t			*mlx;
-	mlx_image_t		*img;
-	mlx_texture_t	*texture;
+	t_game	game;
+	// mlx_t	*mlx;
+	t_img_data	game_img;
+	// int32_t			i;
 
-	mlx = mlx_init(WIDTH, HEIGHT, "so_long", true);
-	if (!mlx)
+	// i = 0;
+	game.mlx = mlx_init(WIDTH, HEIGHT, "so_long", true);
+	if (!game.mlx)
 		ft_error();
-	texture = mlx_load_png("./src/img/chopper_L.png");
-	if (!texture)
+	game_img.texture = mlx_load_png("./src/img/chopper_L.png");
+	if (!(game_img.texture))
 		ft_error();
-	ft_printf("Width: %d\nHeight: %d\n", texture->width, texture->height);
-	img = mlx_texture_to_image(mlx, texture);
-	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
+	ft_printf("Width: %d\nHeight: %d\n", game_img.texture->width, game_img.texture->height);
+	game_img.img = mlx_texture_to_image(game.mlx, game_img.texture);
+	if (!game_img.img || (mlx_image_to_window(game.mlx, game_img.img, WIDTH_TILE * 0, HEIGHT_TILE * 0) < 0))
 		ft_error();
-	if (!img || (mlx_image_to_window(mlx, img, 0, 128) < 0))
-		ft_error();
-	if (!img || (mlx_image_to_window(mlx, img, 128, 0) < 0))
-		ft_error();
-	if (!img || (mlx_image_to_window(mlx, img, 128, 128) < 0))
-		ft_error();
-	mlx_key_hook(mlx, &my_keyhook, NULL);
-	mlx_loop(mlx);
-	mlx_delete_image(mlx, img);
-	mlx_delete_texture(texture);
-	mlx_terminate(mlx);
+	// if (!img || (mlx_image_to_window(mlx, img, 0, 128) < 0))
+	// 	ft_error();
+	// if (!img || (mlx_image_to_window(mlx, img, 5000, 0) < 0))
+	// 	ft_error();
+	// if (!img || (mlx_image_to_window(mlx, img, 128, 128) < 0))
+	// 	ft_error();
+	mlx_key_hook(game.mlx, &my_keyhook, &game_img);
+	// mlx_loop_hook(mlx, &ft_count_loop, &i);
+	mlx_loop(game.mlx);
+	mlx_delete_image(game.mlx, game_img.img);
+	mlx_delete_texture(game_img.texture);
+	mlx_terminate(game.mlx);
 	return (EXIT_SUCCESS);
 }
