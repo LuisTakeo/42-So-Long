7b2294bc98ecfd5abbc215aa1c9c61837213923b
LuisTakeo <luistakeo@hotmail.com>
2024-02-02 02:55:57 -0300

    Adição de \r na parte da validação

--- src/char_valid.c
+++ src/char_valid.c
@@ -12,35 +12,38 @@
 
 #include "../includes/so_long.h"
 
-void	verify_char_valid(char **map)
+void verify_char_valid(char **map)
 {
-	int32_t	l;
-	int32_t	c;
+	int l;
+	int c;
 
-	l = -1;
-	while (map[++l])
+	l = 0;
+	c = 0;
+	while (map[l])
 	{
-		c = -1;
-		while (map[l][++c] && map[l][c] != '\n')
+		c = 0;
+		while (map[l][c])
 		{
-			if (!ft_strchr("1E0CP", map[l][c]))
+			if (!ft_strchr("1E0CP\n\r", map[l][c]))
 			{
+				ft_printf("Caractere inválido: %d\n Linha %d, coluna %d.\n", map[l][c], l, c);
 				free_map(map);
-				exit (ft_printf("Não possui todos caracteres válidos."));
+				exit(ft_printf("Não possui todos caracteres válidos."));
 			}
+			c++;
 		}
+		l++;
 	}
-	if (map[l - 1][c] == '\n')
+	if (map[l - 1][c] == '\n' || map[l - 1][c] == '\r')
 	{
 		free_map(map);
-		exit (ft_printf("Quebra de linha no final"));
+		exit(ft_printf("Quebra de linha no final"));
 	}
-
 }
 
-void	verify_min_char(char **map, t_valid_map *t_map)
+void verify_min_char(char **map, t_valid_map *t_map)
 {
-	int32_t	l;
+	int32_t l;
 
 	l = -1;
 	while (map[++l])
@@ -49,26 +52,24 @@ void	verify_min_char(char **map, t_valid_map *t_map)
 		t_map->collectible_occ += count_occ(map[l], 'C');
 		t_map->exit_occ += count_occ(map[l], 'E');
 	}
-	if (t_map->ply_occ != 1 || t_map->exit_occ != 1
-		|| t_map->collectible_occ == 0)
+	if (t_map->ply_occ != 1 || t_map->exit_occ != 1 || t_map->collectible_occ == 0)
 	{
 		free_map(map);
-		exit (ft_printf("Quantia inválida de Player, Exit e/ou Coletáveis"));
+		exit(ft_printf("Quantia inválida de Player, Exit e/ou Coletáveis"));
 	}
 }
 
-void	verify_walls(char **map, t_valid_map *t_map)
+void verify_walls(char **map, t_valid_map *t_map)
 {
-	int32_t	lst_line;
-	int32_t	lst_column;
-	int32_t	i;
-	int32_t	occ_invalid;
+	int32_t lst_line;
+	int32_t lst_column;
+	int32_t i;
+	int32_t occ_invalid;
 
 	lst_column = t_map->last_col - 1;
 	lst_line = t_map->last_line - 1;
 	occ_invalid = 0;
-	if ((count_occ(map[0], '1') != t_map->last_col)
-		|| (count_occ(map[lst_line], '1') != t_map->last_col))
+	if ((count_occ(map[0], '1') != t_map->last_col) || (count_occ(map[lst_line], '1') != t_map->last_col))
 		occ_invalid = 1;
 	i = -1;
 	while (map[++i])
